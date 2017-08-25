4b52cb6606be4b5ff01e456c1ee4d8957e05bdb2
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-08-25 15:15:48 -0400

    Update get_course_list.js

--- js/get_course_list.js
+++ js/get_course_list.js
@@ -1,7 +1,7 @@
 /* Used to create a list of courses for plh.io/major-finder */
 var course_list = [];
 function get_list(){
-  $.getJSON("https://raw.githubusercontent.com/patrickleweryharris/major-finder/master/json/utm.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/patrickleweryharris/major-finder/master/json/majors.json", function(data){
     for (i = 0; i < data.length; i++){
       course_list = course_list.concat(data[i].requirements);
     }
