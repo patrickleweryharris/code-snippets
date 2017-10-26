414cf17a49490f468df35b0465acb00a3fefde94
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-10-26 13:26:47 -0400

    Moved scripts to their own repo

--- scripts/convert-m4v.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in *.m4v;
-  do name=`echo $i | cut -d'.' -f1`;
-  echo $name;
-  ffmpeg -ss 00:00:05 -i "$i" -frames:v 1 "./output/$name.png"
-  ffmpeg -i "$i" "./output/$name.mp4";
-done
--- scripts/youtube-download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    youtube-dl $line
-done <"$1"
